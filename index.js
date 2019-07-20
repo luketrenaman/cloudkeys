d3e35caa70df8f6d28b756b828a02827dcc6e20e
Luke Trenaman <trenamanluke@gmail.com>
2019-07-20 19:22:00 +0100

    progress?

--- index.js
+++ index.js
@@ -3,37 +3,49 @@ var fs = require("fs");
 var clients = [];
 var enabled = false;
 var input = new midi.input();
-var data = JSON.parse(fs.readFileSync("midi.json"));
-function poll(){
-	fs.writeFileSync("midi.json",JSON.stringify(data))
-	
-	data = JSON.parse(fs.readFileSync("midi.json"))
-	if(input.getPortCount() < 2){
-		//Set to false so that when the port count reaches 2, the else condition will not open the port multiple times
-		if(enabled){
-			try{input.closePort(1);}
-			catch{}
-		}
-		enabled = false
-		
-		//In this position only the midi cable is connected
-		console.log("awaiting");
-	} else{
-		//In this position both the piano and midi cable are connected... The logic here needs to figure out what ports are still open
-		if(!enabled){
-input = new midi.input()
-input.on('message',function(deltaTime,message){
-	console.log(message);	
-	message.push(Date.now());
-	data.push(message)
-});
+var data = JSON.parse(fs.readFileSync("midi2.json"));
+var anal = JSON.parse(fs.readFileSync("anal.json"));
 
-			enabled = true;
-			input.openPort(1);
+function poll() {
+    fs.writeFileSync("midi2.json", JSON.stringify(data))
+    fs.writeFileSync("anal.json",JSON.stringify(anal))
+    data = JSON.parse(fs.readFileSync("midi.json"))
+    if (input.getPortCount() < 2) {
+        //Set to false so that when the port count reaches 2, the else condition will not open the port multiple times
+        if (enabled) {
+            try {
+                input.closePort(1);
+            } catch {}
+        }
+        enabled = false
+
+        //In this position only the midi cable is connected
+        //console.log("awaiting");
+    } else {
+        //In this position both the piano and midi cable are connected... The logic here needs to figure out what ports are still open
+        if (!enabled) {
+            input = new midi.input()
+            input.on('message', function(deltaTime, message) {
+                //calculations
+		if (message[2] > 0){
+		var keyVal = message[1] - 21;
+		anal[keyVal]++;
 		}
-	}
-	console.log(input.getPortCount())
-	setTimeout(poll,500)
+		console.log(message);
+                clients.forEach(function(client) {
+                    client.emit('messages', message)
+                });
+
+                message.push(Date.now());
+                data.push(message)
+            });
+
+            enabled = true;
+            input.openPort(1);
+        }
+    }
+    //console.log(input.getPortCount())
+    setTimeout(poll, 500)
 }
 poll()
 
@@ -47,17 +59,20 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 //app.use(express.static(__dirname + '/tonejs-instruments'));
-
-//app.get('/', function(req, res,next) {
-//    res.sendFile(__dirname + '/index.html');
-//});
+var clients = []
+app.get('/', function(req, res, next) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/data', function(req, res, next) {
+    res.sendFile(__dirname + 'anal.json');
+});
 io.on('connection', function(client) {
+    client.on('join', function(data) {
 
-	client.on('join',function(data) {
-		console.log(data);
-		clients.push(client);
-		client.emit('messages','Hello from server');
-	});
+        console.log(data);
+        clients.push(client);
+        client.emit('messages', 'Hello from server');
+
+    });
 });
 server.listen(3000);
-
